ed51f0baa4771818ac3fefc60db400f482878333
zm00622 <zarymanning@gmail.com>
2021-08-21 16:13:45 -0400

    First Commit

---
+++ src/components/add-tutorial.component.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import TutorialDataService from "../services/tutorial.service";
+
+export default class AddTutorial extends Component {
+  constructor(props) {
+    super(props);
+    this.onChangeTitle = this.onChangeTitle.bind(this);
+    this.onChangeDescription = this.onChangeDescription.bind(this);
+    this.saveTutorial = this.saveTutorial.bind(this);
+    this.newTutorial = this.newTutorial.bind(this);
+
+    this.state = {
+      title: "",
+      description: "",
+      published: false,
+
+      submitted: false,
+    };
+  }
+
+  onChangeTitle(e) {
+    this.setState({
+      title: e.target.value,
+    });
+  }
+
+  onChangeDescription(e) {
+    this.setState({
+      description: e.target.value,
+    });
+  }
+
+  saveTutorial() {
+    let data = {
+      title: this.state.title,
+      description: this.state.description,
+      published: false
+    };
+
+    TutorialDataService.create(data)
+      .then(() => {
+        console.log("Created new item successfully!");
+        this.setState({
+          submitted: true,
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
+
+  newTutorial() {
+    this.setState({
+      title: "",
+      description: "",
+      published: false,
+
+      submitted: false,
+    });
+  }
+
+  render() {
+    return (
+      <div className="submit-form">
+        {this.state.submitted ? (
+          <div>
+            <h4>You submitted the client successfully!</h4>
+            <br></br>
+            <h4>You can now see your new client on the clients page.</h4>
+            <button className="btn btn-success" onClick={this.newTutorial}>
+              Add
+            </button>
+          </div>
+        ) : (
+          <div>
+            <div className="form-group">
+              <label htmlFor="title">Client Name</label>
+              <input
+                type="text"
+                className="form-control"
+                id="title"
+                required
+                value={this.state.title}
+                onChange={this.onChangeTitle}
+                name="title"
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Address and Contact Information</label>
+              <textarea
+                type="text"
+                className="form-control"
+                id="description"
+                required
+                value={this.state.description}
+                onChange={this.onChangeDescription}
+                name="description"
+              />
+            </div>
+
+            <button onClick={this.saveTutorial} className="btn btn-success">
+              Submit
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
